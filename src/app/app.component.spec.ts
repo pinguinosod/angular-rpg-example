54dafcb0f62a3da5bd7cae63488606d4a3f8aaeb
PinguinoSod <pinguino.sod@gmail.com>
2019-09-18 20:09:48 +0200

    fix the one and only existing test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,41 @@
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { AdventureComponent } from './adventure/adventure.component';
+import { CreateHeroComponent } from './heroes/create-hero/create-hero.component';
+import { EncounterComponent } from './adventure/encounter/encounter.component';
+import { FighterComponent } from './adventure/encounter/fighter/fighter.component';
+import { DebugToolsComponent } from './debug-tools/debug-tools.component';
+import { DebugGetStateComponent } from './debug-tools/debug-get-state/debug-get-state.component';
+import { DebugSetStateComponent } from './debug-tools/debug-set-state/debug-set-state.component';
+
+import { StateService } from './state.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        FormsModule
+      ],
+      providers: [StateService],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        HeroesComponent,
+        HeroComponent,
+        CreateHeroComponent,
+        AdventureComponent,
+        EncounterComponent,
+        FighterComponent,
+        DebugToolsComponent,
+        DebugGetStateComponent,
+        DebugSetStateComponent
       ],
     }).compileComponents();
   }));
@@ -16,16 +46,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-rpg-example'`, () => {
+  it(`should have partyEmbarked as false`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-rpg-example');
+    expect(app.partyEmbarked).toBeFalsy();
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-rpg-example!');
+    expect(compiled.querySelector('h1').textContent).toContain('RPG Heroes');
   });
 });
