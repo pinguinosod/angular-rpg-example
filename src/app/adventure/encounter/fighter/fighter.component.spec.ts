b40fb058ee87b30cf99adcda0896e39d9dd0fd5e
PinguinoSod <pinguino.sod@gmail.com>
2019-09-20 23:00:09 +0200

    add test for fighter component

---
+++ src/app/adventure/encounter/fighter/fighter.component.spec.ts
@@ -0,0 +1,57 @@
+import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { FighterComponent } from './fighter.component';
+import { Fighter } from './../fighter';
+import { Party } from './../party.enum';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        FormsModule
+      ],
+      declarations: [
+        FighterComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the component', () => {
+    const fixture = TestBed.createComponent(FighterComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render fighter with name Stuart in a h3 tag', () => {
+    const fixture = TestBed.createComponent(FighterComponent);
+    fixture.componentInstance.fighter = {
+      id: 1, name: 'Stuart', party: Party.FRIEND, minAttack: 10, maxAttack: 20, hpCurrent: 50, hpMax: 100
+    };
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h3').textContent).toContain('Stuart');
+  });
+
+  it('should render fighter with name Camila in a h3 tag', () => {
+    const fixture = TestBed.createComponent(FighterComponent);
+    fixture.componentInstance.fighter = {
+      id: 1, name: 'Camila', party: Party.FRIEND, minAttack: 10, maxAttack: 20, hpCurrent: 50, hpMax: 100
+    };
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h3').textContent).toContain('Camila');
+  });
+
+  it('should render fighter with hp 20/60 in a td tag', () => {
+    const fixture = TestBed.createComponent(FighterComponent);
+    fixture.componentInstance.fighter = {
+      id: 1, name: 'Dani', party: Party.FRIEND, minAttack: 10, maxAttack: 20, hpCurrent: 20, hpMax: 60
+    };
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('td').textContent).toContain('20 / 60');
+  });
+});
